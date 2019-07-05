a8910c1515eb80b60b966dbebcb6cf388d1754d8
Huynh Vu <vdhuynh@tma.com.vn>
2019-07-05 15:19:12 +0700

    - Update the users table - Write the UsersTableSeeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+    }
+}
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => "Nguyen Van A",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van B",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van C",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van D",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van E",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van F",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van I",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ],
+            [
+                'name' => "Nguyen Van K",
+                'email' => str_random(6) . '@example.com',
+                'password' => bcrypt('matkhau')
+            ]
+        ]);
     }
 }
