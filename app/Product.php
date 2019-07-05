9ad3143683692a397db6f5292621dfeac7a6a6d8
Huynh Vu <vdhuynh@tma.com.vn>
2019-07-05 17:04:24 +0700

    - Create Category, Product Models

---
+++ app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = "products";
+
+    public function category(){
+        return $this->belongsTo('\App\Category', 'category_id', 'id');
+    }
+
+    public function user(){
+        return $this->belongsTo('\App\User','created_user_id','id');
+    }
+}
